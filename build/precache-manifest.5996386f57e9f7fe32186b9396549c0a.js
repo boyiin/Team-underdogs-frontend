d6d2645e14ab170638c777149cb6e35645e15892
James Obande <Jamesobande2015@gmail.com>
2020-06-05 14:42:21 +0100

    feat: created reactapp

---
+++ build/precache-manifest.5996386f57e9f7fe32186b9396549c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1052dfe5e652f5c29ab286463cd2dccf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c6a2ab123c9386c0906",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d594bfdf5d1fb65977c",
+    "url": "/static/js/2.871422f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.871422f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6a2ab123c9386c0906",
+    "url": "/static/js/main.da70e994.chunk.js"
+  },
+  {
+    "revision": "faf7c69fe51b40c2a1dd",
+    "url": "/static/js/runtime-main.7547c06a.js"
+  }
+]);
